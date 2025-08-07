a7899abc85ace33f85cf7416e980b83ed7e0a9bf
Kira-ayush <aayush.kr.gope@gmail.com>
2025-08-07 13:11:44 +0530

    foreign key migrations

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
+use App\Http\Resources\ProjectCollection;
 use App\Http\Resources\ProjectResource;
 use App\Models\Project;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ProjectController extends Controller
 {
+    public function index(Request $request)
+    {
+        $projects = QueryBuilder::for(Project::class)
+            ->allowedIncludes('tasks')
+            ->paginate();
+
+        return new ProjectCollection($projects);
+    }
+
     public function store(StoreProjectRequest $request)
     {
         $validated = $request->validated();
@@ -19,6 +31,11 @@ class ProjectController extends Controller
         return new ProjectResource($project);
     }
 
+    public function show(Request $request, Project $project)
+    {
+        return (new ProjectResource($project))->load('tasks');
+    }
+
     public function update(UpdateProjectRequest $request, Project $project)
     {
         $validated = $request->validated();
@@ -26,5 +43,13 @@ class ProjectController extends Controller
         $project->update($validated);
 
         return new ProjectResource($project);
+    }
+
+    public function destroy(Request $request, Project $project)
+    {
+        
+        $project->delete();
+
+        return response()->noContent();
     }
 }
